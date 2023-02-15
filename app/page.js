58a627b66464312126d129ca4dd027a693532fd3
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-02-16 00:41:45 +0530

    Scroll snap toh ho raha hai

--- app/page.js
+++ app/page.js
@@ -4,12 +4,16 @@ import Text from './Text';
 export default function Home() {
   return (
     <>
-      <div className="snap-y">
-        <div className="snap-center absolute h-screen top-0">
+      <div className="snap snap-y h-screen w-screen overflow-x-hidden snap-mandatory overflow-scroll">
+        <div className="snap-center h-screen">
           <ThreeD />
         </div>
-        <div className="snap-center h-screen">
+        <div className="snap-center h-screen bg-slate-600">
           <Text />
+        </div>
+        <div className="snap-center h-screen bg-yellow-300">
+          <Text />
+          sdjvkbhslvhbkvbkvjb
         </div>
       </div>
     </>
