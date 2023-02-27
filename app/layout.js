f032b85e413225ee8d80622109ea8e6fa7157d1e
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-02-27 23:46:44 +0530

    HUHULULLU

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import './globals.css'
 
+export const metadata = {
+  title: 'Portoflio',
+  description: "Portoflio website"
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
--- app/page.js
+++ app/page.js
@@ -1,14 +1,19 @@
 import ThreeD from './ThreeD'
 import Text from './Text';
+import { Allerta_Stencil } from '@next/font/google'
 
+
+const allerta = Allerta_Stencil({ subsets: ['latin'], weight: ['400'], variable: '--font-Alerta' })
 export default function Home() {
+
   return (
     <>
       <div className="overflow-x-hidden">
         <div className="bg-gradient-to-b from-black to-gray-900">
           <ThreeD />
-          <div className="top-2/3 absolute">
-            <Text />
+          <div className="top-3/4 absolute">
+            {/* <Text /> */}
+            <p className={`font-alerta ${allerta.variable} text-7xl text-gray-200`}>Hello There</p>
           </div>
           <div className="h-screen"></div>
         </div>
