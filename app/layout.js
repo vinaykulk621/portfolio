ea77f7c6ddec1af9274c020b2f1771d6e10f454c
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-11 22:51:41 +0530

    Hardly Workin'

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head />
       <body className="overflow-x-hidden">{children}</body>
     </html>
   )
