0a44b5c04f44b3d599edb356f7b2b7f64ca61bc0
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-11 23:34:17 +0530

    .

--- components/ThreeD.js
+++ components/ThreeD.js
@@ -6,40 +6,40 @@ import { ShaderMaterial } from "three"
 
 // import { GUI } from "dat.gui";
 
+const vertexShader = `
+  varying vec2 vertexUV;
+  varying vec3 vertexNormal;
+  void main() {
+    vertexUV=uv;
+    vertexNormal=normalize(normalMatrix*normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+  `
+const atmosphereVertexShader = `
+  varying vec3 vertexNormal;
+  void main() {
+    vertexNormal=normalize(normalMatrix*normal);
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 0.9);
+  }
+  `
+const fragmentShader = `
+  uniform sampler2D globeTexture;
+  varying vec2 vertexUV;
+  varying vec3 vertexNormal;
+  void main(){
+    float intensity=1.05-dot(vertexNormal,vec3(0.0,0.0,1.0));
+    vec3 atmosphere=vec3(0.3,0.6,1.0)*pow(intensity,1.5);
+    gl_FragColor = vec4(atmosphere+texture2D(globeTexture,vertexUV).xyz,1.0);
+  }
+`
+const atmosphereFragmentShader = `
+  varying vec3 vertexNormal;
+  void main(){
+    float intensity=pow(0.4-dot(vertexNormal,vec3(0,0,1.0)),2.0);
+    gl_FragColor = vec4(0.3,0.6,1.0,1.0)*intensity;
+  }
+`
 const ThreeD = () => {
-  const vertexShader = `
-    varying vec2 vertexUV;
-    varying vec3 vertexNormal;
-    void main() {
-      vertexUV=uv;
-      vertexNormal=normalize(normalMatrix*normal);
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-    }
-    `
-  const atmosphereVertexShader = `
-    varying vec3 vertexNormal;
-    void main() {
-      vertexNormal=normalize(normalMatrix*normal);
-      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 0.9);
-    }
-    `
-  const fragmentShader = `
-    uniform sampler2D globeTexture;
-    varying vec2 vertexUV;
-    varying vec3 vertexNormal;
-    void main(){
-      float intensity=1.05-dot(vertexNormal,vec3(0.0,0.0,1.0));
-      vec3 atmosphere=vec3(0.3,0.6,1.0)*pow(intensity,1.5);
-      gl_FragColor = vec4(atmosphere+texture2D(globeTexture,vertexUV).xyz,1.0);
-    }
-  `
-  const atmosphereFragmentShader = `
-    varying vec3 vertexNormal;
-    void main(){
-      float intensity=pow(0.4-dot(vertexNormal,vec3(0,0,1.0)),2.0);
-      gl_FragColor = vec4(0.3,0.6,1.0,1.0)*intensity;
-    }
-  `
   const sceneRef = useRef(null)
   const cameraRef = useRef(null)
   const rendererRef = useRef(null)
