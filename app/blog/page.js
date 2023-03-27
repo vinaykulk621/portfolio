353d1bd924a5b31d33657d5e561c650ab6364008
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-27 23:19:59 +0530

    Clicks Done Good

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,7 @@
 export default function Home(params) {
   return (
     <>
-      <p className="flex h-screen w-screen items-center justify-center bg-black font-spotify text-9xl text-white">
+      <p className="flex h-screen w-screen items-center justify-center bg-black font-spotify text-xl text-white xs:text-4xl md:text-6xl lg:text-9xl">
         Hardly Working On it
       </p>
     </>
--- app/page.js
+++ app/page.js
@@ -7,21 +7,23 @@ import ThreeD from "../components/ThreeD"
 export default function Home() {
   return (
     <>
-      {/* <ThreeD /> */}
-      <div className="t bg-gradient-to-b from-simple-black-2 to-simple-black font-spotify">
-        <div className="h-screen">
-          <Intro />
+      <div className="overflow-x-hidden">
+        {/* <ThreeD /> */}
+        <div className="t bg-gradient-to-b from-simple-black-2 to-simple-black font-spotify">
+          <div className="h-screen">
+            <Intro />
+          </div>
+          <div className="flex h-screen flex-col items-center justify-center space-y-10 p-4 lg:flex-row lg:space-x-10">
+            <TechStack />
+            <AboutMe />
+          </div>
+          <Projects />
+          <p className="relative bottom-[20%] text-center text-lg text-gray-400">
+            Check out this{" "}
+            <span className="text-gray-500 hover:text-gray-400">COOL BALL</span>{" "}
+            down here that i made
+          </p>
         </div>
-        <div className="flex h-screen flex-col items-center justify-center space-y-10 p-4 lg:flex-row lg:space-x-10">
-          <TechStack />
-          <AboutMe />
-        </div>
-        <Projects />
-        <p className="relative bottom-[20%] text-center text-lg text-gray-400">
-          Check out this{" "}
-          <span className="text-gray-500 hover:text-gray-400">COOL BALL</span>{" "}
-          down here that i made
-        </p>
       </div>
     </>
   )
