1985695e15f6dfcd4165165def0d18261990d442
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-03-30 11:20:13 +0530

    Fix Mistake in layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="overflow-x-hidden">
         {children}
         <Analytics />
       </body>
