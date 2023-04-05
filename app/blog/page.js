344bdd2b11c3c99a2b7b8da699eb8f41f1e711ac
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-04-05 23:19:00 +0530

    Something

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,6 +1,33 @@
+// import Link from 'next/link'
+// import getPostMetadata from '../components/getPostMetadata'
+
 export default function Home(params) {
+  // const postMetadata = getPostMetadata()
+  // const postPreviews = postMetadata.map((post) => {
+  //   return (
+  //     <>
+  //       <div
+  //         className="rounded-md border border-slate-300 bg-white p-4
+  //   shadow-sm"
+  //       >
+  //         <p className="text-sm text-slate-400">{props.date}</p>
+
+  //         <Link href={`/posts/${props.slug}`}>
+  //           <h2 className=" mb-4 text-violet-600 hover:underline">
+  //             {props.title}
+  //           </h2>
+  //         </Link>
+  //         <p className="text-slate-700">{props.subtitle}</p>
+  //       </div>
+  //     </>
+  //   )
+  // })
+
   return (
     <>
+      {/* <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+        {postPreviews}
+      </div> */}
       <p className="flex h-screen w-screen items-center justify-center bg-black font-spotify text-xl text-white xs:text-4xl md:text-6xl lg:text-9xl">
         Hardly Working On it
       </p>
--- components/SVG.js
+++ components/SVG.js
@@ -1,8 +1,6 @@
-import Link from 'next/link'
-
 export function BackButton({}) {
   return (
-    <Link href={'/'}>
+    <a href={'/'}>
       <div className="absolute left-2 top-2 rounded-xl bg-gray-900 px-4 py-2 text-center font-spotify hover:bg-gray-800">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +13,7 @@ export function BackButton({}) {
           <path d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
         </svg>
       </div>
-    </Link>
+    </a>
   )
 }
 
