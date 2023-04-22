132c77e88ae1116f79b9966055cd089b4119504f
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-04-22 13:23:46 +0530

    Next js 13.3.1

--- components/Projects/ProjectPallet.js
+++ components/Projects/ProjectPallet.js
@@ -19,16 +19,16 @@ export function ProjectPallet({
           alt={alt}
           className="w-full rounded-2xl rounded-b-lg p-2"
         />
+        <div className="flex space-x-2 px-4">
+          <GithubSVG url={github_repo_name} />
+          {DemoUrl !== undefined ? <Redirect DemoUrl={DemoUrl} /> : ''}
+        </div>
         <div className="flex h-fit flex-col justify-evenly space-y-3 px-1 py-2">
           <div className="px-2 text-4xl underline-offset-4 group-hover:underline">
             {projectName}
           </div>
           <div className="text-ellipsis break-words px-2 text-xl">
             {projectDescription}
-          </div>
-          <div className="flex space-x-2 px-2">
-            <GithubSVG url={github_repo_name} />
-            {DemoUrl !== undefined ? <Redirect DemoUrl={DemoUrl} /> : ''}
           </div>
         </div>
       </div>
