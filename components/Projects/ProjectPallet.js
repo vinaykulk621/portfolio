b4aa6cfc2831ad38b5bc53503361a210572bc23a
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-04-24 21:03:10 +0530

    Okay

--- components/Projects/ProjectPallet.js
+++ components/Projects/ProjectPallet.js
@@ -10,28 +10,26 @@ export function ProjectPallet({
   alt,
 }) {
   return (
-    <>
-      <div className="group h-full max-w-sm overflow-hidden rounded-b-lg rounded-t-2xl bg-neutral-900 transition-all hover:bg-neutral-800">
-        <Image
-          src={`/projects/${Preview}`}
-          height={300}
-          width={300}
-          alt={alt}
-          className="w-full rounded-2xl rounded-b-lg p-2"
-        />
-        <div className="flex space-x-2 px-4">
-          <GithubSVG url={github_repo_name} />
-          {DemoUrl !== undefined ? <Redirect DemoUrl={DemoUrl} /> : ''}
+    <div className="group h-full max-w-sm overflow-hidden rounded-b-lg rounded-t-2xl bg-neutral-900 transition-all hover:bg-neutral-800">
+      <Image
+        src={`/projects/${Preview}`}
+        height={300}
+        width={300}
+        alt={alt}
+        className="w-full rounded-2xl rounded-b-lg p-2"
+      />
+      <div className="flex space-x-2 px-4">
+        <GithubSVG url={github_repo_name} />
+        {DemoUrl !== undefined ? <Redirect DemoUrl={DemoUrl} /> : ''}
+      </div>
+      <div className="flex h-fit flex-col justify-evenly space-y-3 px-1 py-2">
+        <div className="px-2 text-4xl underline-offset-4 group-hover:underline">
+          {projectName}
         </div>
-        <div className="flex h-fit flex-col justify-evenly space-y-3 px-1 py-2">
-          <div className="px-2 text-4xl underline-offset-4 group-hover:underline">
-            {projectName}
-          </div>
-          <div className="text-ellipsis break-words px-2 text-xl">
-            {projectDescription}
-          </div>
+        <div className="text-ellipsis break-words px-2 text-xl">
+          {projectDescription}
         </div>
       </div>
-    </>
+    </div>
   )
 }
