37c826b193cff0db653f0c5b6ae100da3cbce4ca
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-06-29 00:22:44 +0530

    Lint

--- components/blog/PostPreview.js
+++ components/blog/PostPreview.js
@@ -2,11 +2,9 @@ import Link from 'next/link'
 
 export default function PostPreview(props) {
   return (
-    <>
-      <Link href={`/blog/${props.slug}`}>
-        <h2 className="text-gray-400 hover:underline text-xl">{props.title}</h2>
-        <p className="text-xs text-slate-400">{props.date}</p>
-      </Link>
-    </>
+    <Link href={`/blog/${props.slug}`}>
+      <h2 className="text-xl text-gray-400 hover:underline">{props.title}</h2>
+      <p className="text-xs text-slate-400">{props.date}</p>
+    </Link>
   )
 }
