576217b4bf137fe175c5b292d59476e670f9c833
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-08-06 00:25:41 +0530

    Probable lineNumbers

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -82,25 +82,24 @@ export default makeSource({
           }
         })
       },
-      [
-        rehypePrettyCode,
-        {
-          theme: 'github-dark',
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: 'text', value: ' ' }]
-            }
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push('line--highlighted')
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ['word--highlighted']
-          },
+
+      rehypePrettyCode,
+      {
+        theme: 'github-dark',
+        onVisitLine(node) {
+          // Prevent lines from collapsing in `display: grid` mode, and allow empty
+          // lines to be copy/pasted
+          if (node.children.length === 0) {
+            node.children = [{ type: 'text', value: ' ' }]
+          }
         },
-      ],
+        onVisitHighlightedLine(node) {
+          node.properties.className.push('line--highlighted')
+        },
+        onVisitHighlightedWord(node) {
+          node.properties.className = ['word--highlighted']
+        },
+      },
       () => (tree) => {
         visit(tree, (node) => {
           if (node?.type === 'element' && node?.tagName === 'div') {
