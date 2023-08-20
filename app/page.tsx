ef94b1f4151cea31b94737269f65b9cf2587357d
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-08-20 14:11:25 +0530

    Ball

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <>
       <div className="overflow-x-hidden">
-        {/* <ThreeD /> */}
+        <ThreeD />
         <div className="bg-zinc-950 font-spotify">
           <div className="flex h-screen flex-col justify-between">
             <Intro />
