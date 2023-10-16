087061fe0ea600918f441b1027ad12778fd580b7
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-10-16 19:41:28 +0530

    Will Be learning responsivenes once and for all in tailwind

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,24 +1,38 @@
 import { EmailSVG, GithubSVG, LinkedinSVG, Pointer, VoltFM } from './SVG'
+import { Libre_Barcode_128_Text, Roboto } from 'next/font/google'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
+const libre_barcode = Libre_Barcode_128_Text({
+  subsets: ['latin'],
+  weight: '400',
+})
+
+const Robo = Roboto({
+  weight: '400',
+  subsets: ['greek', 'vietnamese'],
+})
+
 export function Intro({}) {
   return (
-    <div className="flex flex-col items-center justify-center space-y-3 pt-20">
-      <Link href={'https://vinaykulka.vercel.app/'}>
-        <p className="xs:text-4xl text-3xl text-transparent text-white underline decoration-zinc-600 md:text-6xl">
-          Vinay&apos;s <span className="text-gray-400">web</span>
+    <div className="flex flex-col items-center justify-center space-y-3 pt-20 md:items-start md:justify-start">
+      <Link
+        href={'https://vinaykulka.vercel.app/'}
+        className={`m-0 ${libre_barcode.className} flex justify-start px-14 md:justify-center`}
+      >
+        <p className="text-4xl text-gray-500 duration-200 hover:text-gray-400">
+          Vinay&apos;s web
         </p>
       </Link>
-      <div className="flex flex-col items-center gap-10 md:flex-row">
+      <div className="flex flex-col items-center gap-10 px-10 md:flex-row">
         <Link href={'https://vinaykulka.vercel.app/'}>
           <Image
             className="overflow-hidden rounded-full duration-100 md:grayscale md:hover:grayscale-0"
             src="/me.svg"
             alt="Picture of the Creator"
-            width={210}
-            height={210}
+            width={150}
+            height={150}
             priority
           />
         </Link>
@@ -54,8 +68,10 @@ export function Intro({}) {
           </div>
         </div>
       </div>
-      <div className="xs:space-y-6 mx-10 flex flex-col items-center space-y-2 text-justify lg:items-start">
-        <p className="max-w-xl px-6 text-base text-[#a0a0a0]">
+      <div
+        className={`xs:space-y-6 mx-10 flex flex-col items-center space-y-2 text-justify lg:items-start ${Robo.className}`}
+      >
+        <p className="max-w-xl px-6 text-sm text-[#a0a0a0]">
           Hi, i&apos;m Vinay Kulkarni and I&apos;m enthusiastic about{' '}
           <span className="bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text font-extrabold text-transparent">
             technology
